f0612321428d1f914bab5c464adc14b6c757ac30
Michael Underwood <mike.underwood@gmail.com>
2015-05-26 16:02:09 -0600

    Allow uploading of budget data through admin page

--- client/admin/admin.js
+++ client/admin/admin.js
@@ -1,14 +1,17 @@
 Template.csv_upload.events({
-  'submit #csv_form': function (e) {
-    // jQuery always returns an array of matched elements, so get the first one
-    var file = $('#csv_file')[0].files[0];
+  'submit .csv_form': function (e) {
+    // jQuery always returns an array of matched elements, so get the first one.
+    // Then we're only allowing a single file, so get the "first" of those.
+    var file = $(e.currentTarget).find('input:file')[0].files[0];
+    //var file = $('#csv_file')[0].files[0];
+    var locn = e.currentTarget.id;
 
     Papa.parse(file, {
       header: true,
       dynamicTyping: true,
       skipEmptyLines: true,
       complete: function (results) {
-        console.log("Finished: ", results);
+        Meteor.call('handle_' + locn, results);
       }
     });
   }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,15 @@
 BudgetCodes._ensureIndex('code', {unique: 1});
+BudgetCategories._ensureIndex('code', {unique: 1});
+BudgetCategories._ensureIndex('name', {unique: 1});
 
 Meteor.publish('budget_codes', function () {
   return BudgetCodes.find({});
+});
+
+Meteor.publish('budget_categories', function () {
+  return BudgetCategories.find({});
+});
+
+Meteor.publish('finances', function () {
+  return Finances.find({});
 });
