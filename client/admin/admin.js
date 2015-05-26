60d7d49a3517822c762f68de873109dab680e2d0
Michael Underwood <mike.underwood@gmail.com>
2015-05-25 23:44:14 -0600

    Switch to PapaParse for handling CSV uploads

--- client/admin/admin.js
+++ client/admin/admin.js
@@ -1,16 +1,16 @@
 Template.csv_upload.events({
   // Thanks to https://github.com/RohitRox/js_csv/blob/master/index.html
   'submit #csv_form': function (e) {
-    // jQuery always returns an array of matched elements, so need the first
+    // jQuery always returns an array of matched elements, so get the first one
     var file = $('#csv_file')[0].files[0];
-    var reader = new FileReader();
-    reader.onload = function (file) {
-      var content = file.target.result;
-      var rows = content.split(/[\r\n|\n]+/);
-      for (var i = 0; i < rows.length; i++) {
-        console.log('Got a row: ' + rows[i]);
+
+    Papa.parse(file, {
+      header: true,
+      dynamicTyping: false,
+      skipEmptyLines: true,
+      complete: function (results) {
+        console.log("Finished: ", results);
       }
-    };
-    reader.readAsText(file);
+    });
   }
 });
