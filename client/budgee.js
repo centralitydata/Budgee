3251caf603d8a40f9350c1645a62e7b3df453127
Michael Underwood <mike.underwood@gmail.com>
2015-06-18 21:27:12 -0600

    Handle zero-dollar budgets

--- client/budgee.js
+++ client/budgee.js
@@ -123,26 +123,48 @@ Template.expenses.rendered = function () {
 
   var query_params = this.data;
   var tree_data = FinanceTrees.findOne(query_params);
+  Session.set('municipal_query_params', query_params);
 
   if (tree_data) {
-    Iron.controller().state.set('tree_data_state', 1);
-    Meteor.a4a_functions.draw_treemap(tree_data, '#chart');
+    if (tree_data.value === 0) {
+      Iron.controller().state.set('tree_data_state', 'zero-dollars');
+    } else {
+      Iron.controller().state.set('tree_data_state', 'data-good');
+      Meteor.a4a_functions.draw_treemap(tree_data, '#chart');
+    }
   } else {
-    Iron.controller().state.set('tree_data_state', 0);
+    Iron.controller().state.set('tree_data_state', 'data-missing');
   }
 };
 
 Template.expenses.helpers({
-  errorIfMissing: function () {
-    var noData = (Iron.controller().state.get('tree_data_state') === 0);
+  whichExpenseTemplate: function () {
+    var template = 'empty';
+    var state = Iron.controller().state.get('tree_data_state');
 
-    if (noData) {
-      $('#chart').css('display', 'none');
+    if (state === 'data-good') {
+      template = 'expenses_chart';
+    } else if (state === 'zero-dollars') {
+      template = 'expenses_zero';
+    } else if (state === 'data-missing') {
+      template = 'expenses_missing';
     }
 
-    return noData ? 'expenses_missing' : 'empty';
+    return template;
   }
 });
+
+Template.expenses_zero.helpers({
+  municipality: function () {
+    return name_plus_year(Session.get('municipal_query_params'));
+  }
+});
+
+Template.expenses_missing.helpers({
+  municipality: function () {
+    return name_plus_year(Session.get('municipal_query_params'));
+  }
+})
 
 
 /***************************************************************************
@@ -167,3 +189,24 @@ handle_municipality_form = function (e) {
 
   Router.go('expenses', {municipality_id: municipality_id, year: year});
 };
+
+
+/**** Accepts an object with .municipality_id and .year properties, and
+ **   returns on with .name and .year properties.
+ **/
+name_plus_year = function (query) {
+  var municipality = {
+    name: 'that municipality',
+    year: 'that year'
+  };
+
+  var muni = Municipalities.findOne({municipality_id: query.municipality_id});
+  if (muni && muni.name) {
+    municipality.name = muni.name;
+  }
+  if (query.year) {
+    municipality.year = query.year;
+  }
+
+  return municipality;
+}
